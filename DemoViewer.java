e0ee5b35c6ee4332b2fa47e55ccb4fe142e71734
Aryan Thanki <thankiaryan@gmail.com>
2024-06-22 13:44:50 +1200

    Added Vertex and Triangle class

--- DemoViewer.java
+++ DemoViewer.java
@@ -1,5 +1,13 @@
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.SwingConstants;
 
 public class DemoViewer {
     public static void main(String[] args) {
@@ -17,9 +25,9 @@ public class DemoViewer {
 
         //panel to display render results
         JPanel renderPanel = new JPanel(){
-            public void paintComponentGraphics(Graphics g){
+            public void paintComponent(Graphics g){
                 Graphics2D g2 = (Graphics2D) g;
-                g2.setColor(Color.BLACK);
+                g2.setColor(Color.BLACK); 
                 g2.fillRect(0, 0, getWidth(), getHeight());
 
                 //rendering magic will happen here
@@ -27,5 +35,33 @@ public class DemoViewer {
 
         };
         pane.add(renderPanel, BorderLayout.CENTER);
+
+        frame.setSize(400, 400);
+        frame.setVisible(true);
     }
 }
+
+class Vertex{
+    double x;
+    double y;
+    double z;
+    Vertex(double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+class Triangle{
+    Vertex v1;
+    Vertex v2;
+    Vertex v3;
+    Color color;
+    Triangle(Vertex v1, Vertex v2, Vertex v3, Color color){
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+        this.color = color;
+    }
+}
+
